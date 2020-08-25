8722c90ce0d00996525fb695c97c9287d230c4c3
tyamlak <tyamlak@gmail.com>
2020-08-26 00:48:21 +0300

    chart drawing utils

---
+++ UserProfile/static/js/chart_utils.js
@@ -0,0 +1,73 @@
+function chartCallback(event,array){
+    //console.log(event);
+    console.log(array);
+}
+
+function draw_chart(stat,labels,label="Statistics",chart_type="pie"){
+    var canvas = document.getElementById('myChart')
+    var ctx = canvas.getContext('2d')
+    var myChart = new Chart(ctx, {
+        type: chart_type,
+        data: {
+            labels: labels,
+            datasets: [{
+                label: label,
+                data: stat,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1.2
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            onClick:function(e){
+                var activePoints = myChart.getElementsAtEvent(e);
+                var selectedIndex = activePoints[0]._index;
+                //alert(this.data.datasets[0].data[selectedIndex]);
+                console.log(activePoints)
+                console.log(activePoints[0]._view.label)
+            },
+        }
+    });
+    return myChart
+}
