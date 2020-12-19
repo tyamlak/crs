fa3692e5010d675550986a7b4e977d130f1d39aa
tyamlak <tyamlak@gmail.com>
2020-12-19 19:26:46 +0300

    paginated view for image search results

--- case/views/search.py
+++ case/views/search.py
@@ -3,8 +3,27 @@ from django.shortcuts import render, redirect
 
 import face_recognition, pickle
 
+from django.core.paginator import Paginator
+
+from hashlib import sha256
+import time
+from uuid import uuid4
+
+search_results = {}
+RESULTS_PER_PAGE = 5
+
 def search_image(request):
+	global search_results
 	matched_criminals = []
+	if ('page' in request.GET) and ('hash' in request.GET):
+		page = request.GET.get('page')
+		hash_str = request.GET.get('hash')
+		matched_criminals = search_results.get(hash_str)
+		paginator = Paginator(matched_criminals,RESULTS_PER_PAGE)
+		results = paginator.get_page(page)
+		return render(request,'case/imgsearch_results.html',{
+			'matches':results,'hash':hash_str
+		})
 	if request.POST:
 		file = request.FILES.get('image_file')
 		try:
@@ -23,10 +42,19 @@ def search_image(request):
 			encoding = pickle.loads(ob.encoding)
 			matches = face_recognition.compare_faces(new_encodings,encoding)
 			if True in matches:
-				matched_criminals.append((ob.criminal,ob.criminal.images.all()[0]))
+				image_url = '' 
+				if len(ob.criminal.images.all()) > 0:
+					image_url = ob.criminal.images.all()[0].image.url
+					print("Image url is ",image_url)
+				matched_criminals.append((ob.criminal.profile,image_url))
 		if matched_criminals:
+			paginator = Paginator(matched_criminals,RESULTS_PER_PAGE)
+			page = request.GET.get('page')
+			results = paginator.get_page(page)
+			hash_value = sha256((str(time.time()) + str(uuid4()).replace('-','') ).encode()).hexdigest()
+			search_results.update({hash_value:matched_criminals})
 			return render(request,'case/imgsearch_results.html',{
-				'matches': matched_criminals,
+				'matches': results,'hash':hash_value
 			})
 		else:
 			return render(request,'case/search_image.html',{'error':'Image Not found in Database.'})
