cdc34af721a1dc23d0b745e711e3a7ed5e9b4b03
tyamlak <tyamlak@gmail.com>
2021-01-19 20:24:48 +0300

    customize admin site

--- case/admin.py
+++ case/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from case.models import CaseCategory
+from django.contrib.auth.models import Group
 # Register your models here.
 
 admin.site.register((CaseCategory,))
+admin.site.unregister((Group,))
+admin.site.site_header = 'Criminal Recording System Adminstration'
