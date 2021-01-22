3f8e7b5d3821f296c4cf78c611de99764c13ec3c
tyamlak <tyamlak@gmail.com>
2021-01-22 18:36:48 +0300

    test: tests if image is properly flagged

--- case/tests.py
+++ case/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from UserProfile.models import Person, Criminal, CriminalImage
+from .utils import save_image_encodings
+from .init_db import gen_person
+
+
+class FaceRecognitionTest(TestCase):
+
+    def test_image_with_face_is_flagged_true(self):
+        from django.core.files.base import File
+
+        p = gen_person()
+        c = Criminal(profile=p)
+        c.save()
+
+        f = open('case/test_data/face.jpg','rb')
+        image_file = File(f)
+        c_image = CriminalImage(criminal=c,image=image_file)
+        c_image.save()
+
+        save_image_encodings(c_image.pk)
+        assert c_image.has_face == True, "Error in flagging image with face."
--- case/utils.py
+++ case/utils.py
@@ -2,6 +2,10 @@ from django.contrib.auth import get_user
 from case.models import ActivityLog
 from datetime import datetime 
 
+import face_recognition
+from UserProfile.models import CriminalImage, ImageEncoding
+import pickle
+
 
 class Base(object):
 	def __init__(self,func):
@@ -28,3 +32,24 @@ class log_activity(Base):
 		except:
 			print('[DEBUG] Error while saving object')
 		return self.func(*args)
+
+
+def save_image_encodings(image_id):
+	message = ''
+	print("Saving image encoding of id ",image_id)
+	try:
+		ci_model = CriminalImage.objects.get(pk=image_id)
+		image = face_recognition.load_image_file(ci_model.image.file)
+		face_location = face_recognition.face_locations(image)
+		if not len(face_location) or len(face_location)>1:
+			message = 'None or Multiple faces'
+			raise Exception
+		encoding = face_recognition.face_encodings(image,face_location)[0]
+		ImageEncoding(encoding=pickle.dumps(encoding),criminal=ci_model.criminal).save()
+		print("Image Encoding saved!!!")
+	except CriminalImage.DoesNotExist as e:
+		message = 'Image instance not found'
+	except Exception as e:
+		print(e)
+	finally:
+		print(message)
