ba07620a85a494cf81b9a61c5dd7c8b771e59e43
mikiyas tibebu <mikitebe123@gmail.com>
2021-01-24 17:06:38 +0300

    send police info to case-detail

--- case/views/case_mgmt.py
+++ case/views/case_mgmt.py
@@ -42,10 +42,12 @@ def case_detail(request,pk):
 	criminals = case.criminals.all()
 	plaintiff_set = case.plaintiffs.all()
 	witness_set = case.witness_set.all()
+	police_set = case.police_set.all()
 	return render(request,'case/case_detail.html',{
 		'case':case,'criminals':criminals,
 		'plaintiff_set': plaintiff_set,
 		'witness_set': witness_set,
+		'police_set': police_set,
 	}
 	)
 
